03c03b73cb380e36877bd0609c57a25c06261922
SungBin <40740128+sungbin5304@users.noreply.github.com>
2019-05-06 18:43:36 +0900

    Update and rename OnSwipeListener.java to OnSwipeListener.kt
    Change language (java -> kotlin)

---
+++ OnSwipeListener.kt
@@ -0,0 +1,65 @@
+import android.content.Context
+import android.view.GestureDetector
+import android.view.GestureDetector.SimpleOnGestureListener
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnTouchListener
+
+class OnSwipeListener(ctx: Context) : OnTouchListener {
+
+    private val SWIPE_THRESHOLD = 100
+    private val SWIPE_VELOCITY_THRESHOLD = 100
+    private val gestureDetector: GestureDetector
+
+    init {
+        gestureDetector = GestureDetector(ctx, GestureListener())
+    }
+
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        return gestureDetector.onTouchEvent(event)
+    }
+
+    private inner class GestureListener : SimpleOnGestureListener() {
+
+        override fun onDown(e: MotionEvent): Boolean {
+            return true
+        }
+
+        override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+            var result = false
+            try {
+                val diffY = e2.y - e1.y
+                val diffX = e2.x - e1.x
+                if (Math.abs(diffX) > Math.abs(diffY)) {
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            onSwipeLeftToRight()
+                        } else {
+                            onSwipeRightToLeft()
+                        }
+                        result = true
+                    }
+                } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        onSwipeTopToBottom()
+                    } else {
+                        onSwipeBottomToTop()
+                    }
+                    result = true
+                }
+            } catch (exception: Exception) {
+                exception.printStackTrace()
+            }
+
+            return result
+        }
+    }
+
+    fun onSwipeLeftToRight() {}
+
+    fun onSwipeRightToLeft() {}
+
+    fun onSwipeBottomToTop() {}
+
+    fun onSwipeTopToBottom() {}
+}
